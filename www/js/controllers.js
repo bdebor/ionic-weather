32749bd5b9dc06d2e8682fc94c6da98a5125abbe
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-01-22 16:58:47 +0100

    display data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,11 +5,23 @@ angular.module('app.controllers', ['app.services'])
 		$scope.settings = Settings;
 	})
 
-	.controller('WeatherCtrl', function ($scope, $log, Settings, Weather) {
+	.controller('WeatherCtrl', function ($scope, $log, Settings, Weather, $ionicLoading) {
 		$log.info('WeatherCtrl Created');
+
+		$scope.haveData = false;
+		$ionicLoading.show({
+			template: 'Loading...'
+		});
 
 		Weather.getWeatherAtLocation(32.42, -117).then(function (resp) {
 			$log.info(resp);
+			$scope.current = resp.data.currently;
+			$scope.highTemp = Math.ceil(resp.data.daily.data[0].temperatureMax);
+			$scope.lowTemp = Math.floor(resp.data.daily.data[0].temperatureMin);
+			$scope.currentTemp = Math.ceil($scope.current.temperature);
+			$scope.haveData = true;
+			$ionicLoading.hide();
+
 		}, function (error) {
 			alert('Unable to get current conditions');
 			$log.error(error);
